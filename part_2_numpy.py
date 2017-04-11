cd59d4863733e2e9d37aca9cf3627393bea6cdf8
Ronald Seoh <bws@iamblogger.net>
2017-04-11 14:48:30 +0900

    initial commit

---
+++ part_2_numpy.py
@@ -0,0 +1,72 @@
+import theano
+import csv
+import nltk
+import itertools
+import numpy as np
+from rnn_numpy import RNNNumpy
+
+vocabulary_size = 8000
+unknown_token = "UNKNOWN_TOKEN"
+sentence_start_token = "SENTENCE_START"
+sentence_end_token = "SENTENCE_END"
+
+print("Reading CSV file....")
+
+with open('reddit-comments-2015-08.csv', 'r', encoding="utf-8") as reddit_file:
+    reader = csv.reader(reddit_file, skipinitialspace=True)
+    reader.__next__() # To skip the first line?
+
+    # Split full comments into sentences
+    sentences = itertools.chain(*[nltk.sent_tokenize(x[0].lower()) for x in reader])
+    # Append SENTENCE_START and SENTENCE_END
+    sentences = ["%s %s %s" % (sentence_start_token, x, sentence_end_token) for x in sentences]
+
+print("Parsed %d sentences." % (len(sentences)))
+
+tokenized_sentences = [nltk.word_tokenize(sent) for sent in sentences]
+
+word_freq = nltk.FreqDist(itertools.chain(*tokenized_sentences))
+print("Found %d unique words tokens." % len(word_freq.items()))
+
+vocab = word_freq.most_common(vocabulary_size - 1)
+index_to_word = [x[0] for x in vocab]
+index_to_word.append(unknown_token)
+word_to_index = dict([(w, i) for i, w in enumerate(index_to_word)])
+
+print("Using vocabulary size %d." % vocabulary_size)
+print("The least frequent word in our vocabulary is '%s' and appeared %d times." % (vocab[-1][0], vocab[-1][1]))
+
+# Replace all words not in our vocabulary with the unknown token
+for i, sent in enumerate(tokenized_sentences):
+    tokenized_sentences[i] = [w if w in word_to_index else unknown_token for w in sent]
+
+print("\nExample sentence: '%s'" % sentences[0])
+print("\nExample sentence after Pre-processing: '%s'" % tokenized_sentences[0])
+ 
+# Create the training data
+
+# Note that the length of each sentence is different
+# X_train - every words of each sentence except for the last one
+X_train = np.asarray([[word_to_index[w] for w in sent[:-1]] for sent in tokenized_sentences])
+# y_train - every words except for the first one
+y_train = np.asarray([[word_to_index[w] for w in sent[1:]] for sent in tokenized_sentences])
+
+model = RNNNumpy(vocabulary_size)
+o, s = model.forward_propagation(X_train[10]) # 10th training example
+
+print(o.shape)
+print(o)
+
+predictions = model.predict(X_train[10])
+print(predictions.shape)
+print(predictions)
+
+print("Expected loss for random predictions: %f" % np.log(vocabulary_size))
+print("Actual loss: %f" % model.calculate_loss(X_train[:1000], y_train[:1000]))
+
+# To avoid performing millions of expensive calculations we use a smaller vocabulary size for checking.
+grad_check_vocab_size = 100
+np.random.seed(10)
+model = RNNNumpy(grad_check_vocab_size, 10, bptt_truncate=1000)
+model.gradient_check([0,1,2,3], [1,2,3,4])
+
---
+++ rnn_numpy.py
@@ -0,0 +1,184 @@
+import numpy as np
+import operator
+
+def softmax(y):
+
+    e = np.exp(np.array(y))
+
+    return (e/np.sum(e))
+
+class RNNNumpy:
+
+    def __init__(self, word_dim, hidden_dim=100, bptt_truncate=4):
+
+        np.random.seed(10)
+
+        self.word_dim = word_dim
+        self.hidden_dim = hidden_dim
+        self.bptt_truncate = bptt_truncate
+
+        self.U = np.random.uniform(
+                        - np.sqrt(1./word_dim),
+                        np.sqrt(1./word_dim),
+                        (hidden_dim, word_dim)
+                    )
+
+        self.V = np.random.uniform(
+                        - np.sqrt(1./hidden_dim),
+                        np.sqrt(1./hidden_dim),
+                        (word_dim, hidden_dim)
+                    )
+
+        self.W = np.random.uniform(
+                        - np.sqrt(1./hidden_dim),
+                        np.sqrt(1./hidden_dim),
+                        (hidden_dim, hidden_dim)
+                    )
+
+    def forward_propagation(self, x):
+
+        T = len(x)
+
+        # hidden states
+        # this isn't the one that gets trained
+        s = np.zeros((T+1, self.hidden_dim)) # +1 for the last state
+
+        # the last state initialized with zeros 
+        # to calculate the first hidden state
+        s[-1] = np.zeros(self.hidden_dim)
+
+        # (# of words in a single sentence, 8000)
+        o = np.zeros((T, self.word_dim))
+
+        # For every word in this sentence
+        for t in np.arange(T):
+            # state for each word
+            s[t] = np.tanh(
+                # The column of U that covers the word number x[t]
+                # (applies to all the training examples)
+                self.U[:, x[t]] # (hidden_dim, 1)
+
+                # Weight given to previous state
+                + self.W.dot(s[t-1]) # (hidden_dim, 1)
+            )
+
+            # predict the next word on t+1 based on s[t]
+            o[t] = softmax(self.V.dot(s[t])) #(word_dim, 1)
+
+        return [o, s]
+
+    def predict(self, x):
+        o, s = self.forward_propagation(x)
+
+        return np.argmax(o, axis=1)
+
+    def calculate_total_loss(self, x, y):
+
+        L = 0
+
+        for t in np.arange(len(y)):
+
+            o, s = self.forward_propagation(x[t])
+
+            # Note that o was created with the dimension of (T, self.word_dim)
+            # think of this as diagonally going through output!!
+            correct_word_predictions = o[
+                # we can't use ':' here because we are now cherry-picking
+                # the probability assigned to correct answers for each word slot
+                # We use integer array indexing, not slicing 
+                # Check out http://cs231n.github.io/python-numpy-tutorial/
+                np.arange(len(y[t])) # From 0 to (len(y[i]) - 1) = # of words
+                ,y[t] # this isn't scalar; one sentence
+            ]
+
+            # -1 to make the value of loss positive number
+            L += -1 * np.sum(
+                # if the probability assigned to the correct words is low,
+                # this will be way down the negative territory
+                np.log(correct_word_predictions)
+            )
+
+        return L
+
+    def calculate_loss(self, x, y):
+        N = np.sum((len(y_i) for y_i in y))
+
+        return self.calculate_total_loss(x, y) / N
+
+    def bptt(self, x, y):
+        T = len(y)
+
+        o, s = self.forward_propagation(x)
+
+        dLdU = np.zeros(self.U.shape)
+        dLdV = np.zeros(self.V.shape)
+        dLdW = np.zeros(self.W.shape)
+
+        delta_o = o
+
+        delta_o[np.arange(len(y)), y] -= 1.
+
+        for t in np.arange(T)[::1]:
+            dLdV += np.outer(delta_o[t], s[t].T)
+
+            delta_t = self.V.T.dot(delta_o[t]) * (1 - (s[t] ** 2))
+
+            for bptt_step in np.arange(max(0, t-self.bptt_truncate), t+1)[::1]:
+                dLdW += np.outer(delta_t, s[bptt_step - 1])
+                dLdU[:, x[bptt_step]] += delta_t
+
+                delta_t = self.W.T.dot(delta_t) * (1 - s[bptt_step-1] ** 2)
+
+        return [dLdU, dLdV, dLdW]
+
+    def gradient_check(self, x, y, h=0.001, error_threshold=0.01):
+        bptt_gradients = self.bptt(x, y)
+
+        model_parameters = ['U', 'V', 'W']
+
+        for pidx, pname in enumerate(model_parameters):
+            parameter = operator.attrgetter(pname)(self)
+
+            print("Performing gradient check for parameter %s with size %d." % (pname, np.prod(parameter.shape)))
+
+            it = np.nditer(parameter, flags=['multi_index'], op_flags=['readwrite'])
+
+            while not it.finished:
+                ix = it.multi_index
+
+                original_value = parameter[ix]
+
+                parameter[ix] = original_value + h
+                gradplus = self.calculate_total_loss([x], [y])
+                parameter[ix] = original_value - h
+                gradminus = self.calculate_total_loss([x], [y])
+
+                estimated_gradient = (gradplus - gradminus)/(2*h)
+
+                parameter[ix] = original_value
+
+                backprop_gradient = bptt_gradients[pidx][ix]
+
+                relative_error = np.abs(backprop_gradient - estimated_gradient) / (np.abs(backprop_gradient) + np.abs(estimated_gradient))
+
+                if relative_error > error_threshold:
+                    print("Gradient Check ERROR: parameter=%s ix=%s" % (pname, ix))
+                    print("+h Loss: %f" % gradplus)
+                    print("-h Loss: %f" % gradminus)
+                    print("Estimated_gradient: %f" % estimated_gradient)
+                    print("Backpropagation gradient: %f" % backprop_gradient)
+                    print("Relative Error: %f" % relative_error)
+                    return
+
+                it.iternext()
+
+            print("Gradient check for parameter %s passed." % (pname))
+			
+    def numpy_sdg_step(self, x, y, learning_rate):
+        dLdU, dLdV, dLdW = self.bptt(x, y)
+
+        self.U -= learning_rate * dLdU
+		self.V -= learning_rate * dLdV
+		self.W -= learning_rate * dLdW
+
+    def train_with_sgd(model, X_train, y_train, learning_rate=0.005
